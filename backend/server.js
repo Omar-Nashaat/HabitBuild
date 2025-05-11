3660d1c7373e583c71f7e38e7866379e7955f3aa
Omar-Nashaat <omarnashaat18@gmail.com>
2025-05-11 08:47:38 +0300

    add ports

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
 // server.js
-const app = require('./src/app');
-require('dotenv').config();
-const connectDB = require('./src/infrastructure/db/mongo');
+const app = require('./src/app')
+require('dotenv').config()
+const connectDB = require('./src/infrastructure/db/mongo')
 
-const PORT = process.env.PORT || 5000;
-
-
+const PORT = process.env.PORT || 5000
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
-});
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`🚀 Server running on port ${PORT}`)
+  })
+})
